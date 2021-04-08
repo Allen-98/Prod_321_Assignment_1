e790c7e6668936bfe1379ab331bcdba0a0e4f7df
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2021-04-08 18:00:45 +1200

    Fixed the bug of task 2, finished it and start the task 3

---
+++ Assets/Part 2 - Deferred Rendering and Visibility Determination/Task 2/EditVisibilitySphere.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditVisibilitySphere : MonoBehaviour
+{
+
+    bool isChanged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isChanged)
+        {
+            ChangeSize();
+        }
+
+
+    }
+
+    public void ChangeSize()
+    {
+
+        if (transform.Find("VisibilitySphere") != null)
+        {
+
+            Transform vs = transform.Find("VisibilitySphere").transform;
+
+            vs.localScale = new Vector3(7, 7, 7);
+ 
+
+        }
+
+    }
+
+}
