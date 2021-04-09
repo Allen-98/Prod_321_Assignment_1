cc60cb1e86f30204b5ac10a8083e5b46ebe657ea
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2021-04-09 14:12:29 +1200

    Implement Automatically turn the lights off or on while day and night changes

---
+++ Assets/Part 2 - Deferred Rendering and Visibility Determination/Additional Task/LightController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+
+    public float Mult=5;
+    public GameObject sun;
+
+    float angle = 0;
+    float result = 0;
+    public bool isNight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sun.transform.Rotate(Vector3.right * Mult * Time.deltaTime);
+
+        angle += 1 * Mult * Time.deltaTime;
+
+        result = ((angle / 360) - Mathf.Floor(angle / 360)) * 360;
+
+        Debug.Log(result);
+
+        if (result >= 0 && result < 180)
+        {
+            isNight = false;
+        } else if (result >= 180 && result < 360)
+        {
+            isNight = true;
+        }
+
+
+
+
+
+    }
+}
---
+++ Assets/Part 2 - Deferred Rendering and Visibility Determination/Additional Task/LightSourcesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourcesController : MonoBehaviour
+{
+    public LightController lightController;
+
+    public GameObject sun;
+
+    float x;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = sun.transform.rotation.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!lightController.isNight)
+        {
+            //Debug.Log("Day " + sun.transform.localEulerAngles.x);
+            this.gameObject.GetComponent<Light>().enabled = false;
+        }
+        
+        if (lightController.isNight)
+        {
+            //Debug.Log("Night " + sun.transform.localEulerAngles.x);
+            this.gameObject.GetComponent<Light>().enabled = true;        }
+
+    }
+
+
+
+}
